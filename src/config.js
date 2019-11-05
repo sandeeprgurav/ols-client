aa5cf969feafc540111d11d0b491af67e6c71217
Sandeep <sandeeprgurav@gmail.com>
2019-11-04 21:45:21 -0500

    additional changes

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "sandeep-app"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://b34pob8dc7.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_taitUSFxh",
+    APP_CLIENT_ID: "213nmvgirgdceitv1291l4lv2p",
+    IDENTITY_POOL_ID: "us-east-2:1a6a4eaa-83ff-4184-9480-550df2da7317"
+  }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>OLS</h1>
+        <p>Online Leaning Application</p>
+      </div>
+    </div>
+  );
+}
