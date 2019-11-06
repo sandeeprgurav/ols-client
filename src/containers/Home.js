1c8799f33b7c9814705aeaf7aebd55e558586956
Sandeep <sandeeprgurav@gmail.com>
2019-11-06 15:01:59 -0500

    sp1 changes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 import { LinkContainer } from "react-router-bootstrap";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
 
 
 export default function Home(props) {
@@ -52,14 +53,22 @@ function renderNotesList(notes) {
     );
 }
 
-  function renderLander() {
-    return (
-      <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
-      </div>
-    );
-  }
+function renderLander() {
+return (
+  <div className="lander">
+    <h1>Scratch</h1>
+    <p>A simple note taking app</p>
+    <div>
+      <Link to="/login" className="btn btn-info btn-lg">
+        Login
+      </Link>
+      <Link to="/signup" className="btn btn-success btn-lg">
+        Signup
+      </Link>
+    </div>
+  </div>
+);
+}
 
   function renderNotes() {
     return (
